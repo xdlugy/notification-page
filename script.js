0e95120e7c724def181ea25fbe66b7617d3dd71b
Kacper <xdlugy@gmail.com>
2022-10-31 17:27:57 +0100

    value box hides after marking as read

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function change(){
-    let newnot = document.getElementsByClassName('new-notification');
+function read(){
+    let newnot = document.getElementsByClassName('notification new');
     let dot = document.getElementsByClassName('dot');
     for(let i=0; i<3; i++)
-        newnot[0].className='old-notification';
+        newnot[0].className='notification old';
     for(let i=0; i<3; i++)
-        dot[i].style="width: 0; height: 0";
-        document.getElementsByClassName('read')[0].style="color: lightgray; cursor: default";
+        dot[i].style="opacity: 0";
+    document.getElementsByClassName('value')[0].style="opacity: 0";
+        document.getElementsByClassName('read')[0].style="color: lightgray; cursor: default;";
 }
