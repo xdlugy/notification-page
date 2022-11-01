91e8b0f19eca015fc34f1eea039893d210475d57
Kacper <xdlugy@gmail.com>
2022-11-01 20:39:54 +0100

    hide notifications working!

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ const hideall = document.getElementsByClassName('hideall');
 const readall = document.getElementsByClassName('readall');
 const picture = document.getElementsByClassName('picture');
 const text = document.getElementsByClassName('text');
+var hidden=0;
 
-function animation(element,scale){
+function animationhide(element,scale){
     element.style="transform: scale("+scale+")";
     setTimeout(function (){
         element.style="transform: scale(0)";
     }, 300);
+    setTimeout(function (){
+        element.innerHTML="";
+    },600)
 }
 
 function read(){
@@ -22,8 +26,8 @@ function read(){
     for(let i=0; i<3; i++)
         newnot[0].className='notification old';
     for(let i=0; i<3; i++)
-        animation(dot[i],1.3);
-    animation(notivalue[0],1.1);
+        animationhide(dot[i],1.3);
+    animationhide(notivalue[0],1.1);
         readall[0].style="opacity: 0; display: none";
     }
 }
@@ -35,35 +39,51 @@ function drop(){
 }
 
 function hide(){
-    hideall[0].style="display: none";
-    readall[0].style="display: none";
+    hidden=1;
+    dropcontent[0].style="display: none";
     dropbutton[0].style="color: lightgray; border: 1px solid lightgray; cursor: default";
     dropbutton[0].setAttribute("disabled","");
     dropdown[0].style="border: none";
-    animation(notivalue[0],1.1);
+    animationhide(notivalue[0],1.1);
     setTimeout(function(){
         for(i in notification){
             if(notification[0].className=="notification old")
             notification[0].style="z-index: 10;background-color: rgba(136, 136, 136,1);";
             else
-            notification[0].style="z-index: 10;background-color: rgba(60, 124, 233,1);";
+            notification[0].style="z-index: 10;background-color: rgba(100, 170, 255,1);";
             for(j in picture)
                 picture[j].style="display: none";
             for(j in text)
                 text[j].style="display: none";
             if(i>0){
                 for(j in notification[i].children)
-                notification[i].children[j].style="opacity: 0";
+                notification[i].children[j].style="display: none";
             }
             notification[i].style="translate: 0 -"+(70*i)+"px";
         }
         setTimeout(function(){
             for(i in notification){
-                notification[i].style="translate: -100% -"+(70*i)+"px";
+                notification[i].style="translate: -120% -"+(70*i)+"px";
             }
         }, 350);
     }, 350);
+    setTimeout(function(){
+        for(i in notification)
+            notification[i].style="display: none";
+    },1400);
 }
+
+document.addEventListener('click', function handleClickOutsideBox(event) {
+    if(!hidden){
+    if (!dropdown[0].contains(event.target)) {
+        dropdown[0].style="border: none";
+      dropcontent[0].style ="display: none";
+      dropbutton[0].style="color: gray; border: 1px solid gray; cursor: pointer";
+    }
+
+    }
+  });
+
 /*
 const move = e => {
     notification[0].style="transform: translateX(-"+e.offsetX+"px)";
