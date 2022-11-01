d8a753fdeff43b537c64f49ba781ff3631faf8c1
Kacper <xdlugy@gmail.com>
2022-11-01 13:01:15 +0100

    better animations, working on dropdown

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
+const newnot = document.getElementsByClassName('notification new');
+const dot = document.getElementsByClassName('dot');
+const notivalue = document.getElementsByClassName('value');
+const notification = document.getElementsByClassName('notification');
+const dropdown = document.getElementsByClassName('dropdown');
+const dropcontent = document.getElementsByClassName('dropcontent');
+
+function animation(element,scale){
+    element.style="transform: scale("+scale+")";
+    setInterval(disappear,150);
+    function disappear(){
+        element.style="transform: scale(0)";
+    }
+}
+
 function read(){
-    let newnot = document.getElementsByClassName('notification new');
-    let dot = document.getElementsByClassName('dot');
     for(let i=0; i<3; i++)
         newnot[0].className='notification old';
     for(let i=0; i<3; i++)
-        dot[i].style="opacity: 0";
-    document.getElementsByClassName('value')[0].style="opacity: 0";
-        document.getElementsByClassName('read')[0].style="color: lightgray; cursor: default;";
+        animation(dot[i],1.3);
+    animation(notivalue[0],1.1);
+        document.getElementsByClassName('read')[0].style="opacity: 0; display: none";
 }
+
+function drop(){
+    dropdown[0].style="border-radius: 10px";
+    dropcontent[0].style="display: block";
+}
+
+function hide(){
+    dropcontent[0].style="display: none";
+}
+/*
+const move = e => {
+    notification[0].style="transform: translateX(-"+e.offsetX+"px)";
+}
+
+notification.addEventListener("mousedown",move);
+*/
